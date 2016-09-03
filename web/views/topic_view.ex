9e04c9daa8050cae5e80785450cc6f165f6edc8e
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-03 20:01:08 +0200

    Add topic view

---
+++ web/views/topic_view.ex
@@ -0,0 +1,21 @@
+defmodule PhoenixTrello.TopicView do
+  use PhoenixTrello.Web, :view
+
+  def render("index.json", %{owned_topics: owned_topics}) do
+    %{owned_topics: owned_topics}
+  end
+
+  def render("show.json", %{topic: topic}) do
+    topic
+  end
+
+  def render("error.json", %{changeset: changeset}) do
+    errors = Enum.map(changeset.errors, fn {field, detail} ->
+      %{} |> Map.put(field, detail)
+    end)
+
+    %{
+      errors: errors
+    }
+  end
+end
