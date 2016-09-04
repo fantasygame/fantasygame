c6b3ee6eccadcd979c0c440bd3a1bc62cbd8ef6d
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-04 13:47:13 +0200

    Add rendering topics to home index

--- web/static/js/views/home/index.js
+++ web/static/js/views/home/index.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React                from 'react';
+import { connect }          from 'react-redux';
+import classnames           from 'classnames';
 
-export default class HomeIndexView extends React.Component {
+import { setDocumentTitle } from '../../utils';
+import Actions              from '../../actions/topics';
+import TopicRow            from '../../components/topics/row';
+class HomeIndexView extends React.Component {
+  componentDidMount() {
+    setDocumentTitle('Topics');
+
+    const { dispatch } = this.props;
+    dispatch(Actions.fetchTopics());
+  }
+
+  componentWillUnmount() {
+    this.props.dispatch(Actions.reset());
+  }
+
+  _renderOwnedTopics() {
+    const { fetching } = this.props;
+
+    let content = false;
+
+    const iconClasses = classnames({
+      fa: true,
+      'fa-user': !fetching,
+      'fa-spinner': fetching,
+      'fa-spin':    fetching,
+    });
+
+    if (!fetching) {
+      content = (
+        <div className="topics-wrapper">
+          {::this._renderTopics(this.props.ownedTopics)}
+          {/*{::this._renderAddNewTopic()} */}
+        </div>
+      );
+    }
+
+    return (
+      <section>
+        <header className="view-header">
+          <h3><i className={iconClasses} /> My Topics</h3>
+        </header>
+        {content}
+      </section>
+    );
+  }
+
+  _renderTopics(topics) {
+    return topics.map((topic) => {
+      return <TopicRow
+                key={topic.id}
+                dispatch={this.props.dispatch}
+                {...topic} />;
+    });
+  }
+
   render() {
     return (
-      <div>HomeIndexView</div>
-    )
+      <div className="view-container boards index">
+        {::this._renderOwnedTopics()}
+      </div>
+    );
   }
 }
+
+const mapStateToProps = (state) => (
+  state.topics
+);
+
+export default connect(mapStateToProps)(HomeIndexView);
