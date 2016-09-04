134c85acf398221e24492cc597d8a156c235d381
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-04 13:47:24 +0200

    Topics row component

---
+++ web/static/js/components/topics/row.js
@@ -0,0 +1,21 @@
+import React, {PropTypes} from 'react';
+import { push }           from 'react-router-redux';
+
+export default class TopicRow extends React.Component {
+  _handleClick() {
+    console.log(`Clicker ${this.props.id}`);
+  }
+
+  render() {
+    return (
+      <div id={this.props.id} className="topic" onClick={::this._handleClick}>
+        <div className="inner">
+          <h4>{this.props.title}</h4>
+        </div>
+      </div>
+    );
+  }
+}
+
+TopicRow.propTypes = {
+};
